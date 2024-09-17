c929ded72a9b0f487ae7a2ba4ccae7728689f1cd
alessaflores <abbyf7004@gmail.com>
2024-09-16 18:43:44 -0600

    Add files via upload

---
+++ dowhile3.js
@@ -0,0 +1,9 @@
+//Declarar variable
+let num = 0;
+
+do{
+    num = parseInt(prompt("Ingrese un numero mayor que 100: "));
+
+}while (num <= 100)
+
+document.write("Numero ingresado mayor que 100: ", num);
